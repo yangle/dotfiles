278d422fd0cbcce3d83ea515154cc111bde31ac0
Yang-Le <yanglewu@Princeton.EDU>
2013-11-18 12:09:52 -0500

    add ipelet

---
+++ -ipe/ipelets/customize.lua
@@ -0,0 +1,8 @@
+----------------------------------------------------------------------
+-- My customization ipelet: customize.lua
+----------------------------------------------------------------------
+
+prefs.external_editor = "gvim -f %s"
+-- autosave_filename = config.home .. "/%s.autosave"
+prefs.autosave_filename = "/tmp/%s.autosave"
+prefs.browser = "firefox %s"
