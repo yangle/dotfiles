9f8a85ce1693f4efebdbae6a721c84506ad6295f
Yang-Le Wu <yangleuu@gmail.com>
2017-08-05 17:12:34 -0400

    Set ast_node_interactivity to 'all'

--- -ipython/profile_default/ipython_kernel_config.py
+++ -ipython/profile_default/ipython_kernel_config.py
@@ -1,3 +1,6 @@
+## 'all', 'last', 'last_expr' or 'none', specifying which nodes should be run interactively (displaying output from expressions).
+c.InteractiveShell.ast_node_interactivity = 'all'
+
 ## Enable magic commands to be called without the leading %.
 c.InteractiveShell.automagic = False
 
