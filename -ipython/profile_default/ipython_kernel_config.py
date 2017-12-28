3df4bf04e5de7a6f47b5defaa94fa617472f8dd3
Yang-Le Wu <yangleuu@gmail.com>
2017-12-28 14:16:35 -0500

    More ipython configs

--- -ipython/profile_default/ipython_kernel_config.py
+++ -ipython/profile_default/ipython_kernel_config.py
@@ -1,11 +1,18 @@
-## 'all', 'last', 'last_expr' or 'none', specifying which nodes should be run interactively (displaying output from expressions).
+# 'all', 'last', 'last_expr' or 'none', specifying which nodes should be run interactively (displaying output from expressions).
 c.InteractiveShell.ast_node_interactivity = 'last_expr'
 
-## Enable magic commands to be called without the leading %.
+# Enable magic commands to be called without the leading %.
 c.InteractiveShell.automagic = False
 
-## The part of the banner to be printed before the profile
+# The part of the banner to be printed before the profile
 c.InteractiveShell.banner1 = ''
 
-## Do not insert blank line before a prompt
+# Do not insert blank line before a prompt
 c.InteractiveShell.separate_in = ''
+
+# Just exit
+c.InteractiveShell.confirm_exit = False
+
+# Use the unnumbered ">>> " prompts
+from IPython.terminal.prompts import ClassicPrompts
+c.TerminalInteractiveShell.prompts_class = ClassicPrompts
