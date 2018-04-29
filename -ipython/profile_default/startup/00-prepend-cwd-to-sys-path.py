e0a58d727487c4d673a66c2422a23be5a1bd35d2
Yang-Le Wu <yangleuu@gmail.com>
2018-04-28 23:27:40 -0400

    Try harder to ensure cwd is in sys.path

--- -ipython/profile_default/startup/00-prepend-cwd-to-sys-path.py
+++ -ipython/profile_default/startup/00-prepend-cwd-to-sys-path.py
@@ -1,5 +1,6 @@
 # Make sure sys.path starts with current working directory.
-# (Somehow this is not always the case with embedded IPython.)
+# Somehow this is not always the case with embedded IPython in a virtual env.
+# Also, sys.path.insert(0, '') doesn't work reliably -- apparently something in
+# IPython *occasionally* wipes out the '' entry in sys.path.
 import sys
-if sys.path[0] != '':
-    sys.path.insert(0, '')
+sys.path.insert(0, '.')
