db67b8b868acd74c8ff4442db1071275b92cb4e0
Yang-Le Wu <yangleuu@gmail.com>
2022-03-26 21:37:04 -0400

    Replace deprecated magic() with run_line_magic()

--- -ipython/profile_default/startup/05-profilers.py
+++ -ipython/profile_default/startup/05-profilers.py
@@ -1,12 +1,12 @@
 from IPython import get_ipython as _get_ipython
 
 try:
-    _get_ipython().magic("%load_ext line_profiler")
+    _get_ipython().run_line_magic("%load_ext line_profiler")
 except Exception:
     pass
 
 try:
-    _get_ipython().magic("%load_ext memory_profiler")
+    _get_ipython().run_line_magic("%load_ext memory_profiler")
 except Exception:
     pass
 
