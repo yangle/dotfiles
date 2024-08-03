b948927492c11c16556acb1f6aaf115a662cba71
Yang-Le Wu <yangleuu@gmail.com>
2024-08-03 13:31:58 -0400

    Set show_all=True in %pyinstrument

--- -ipython/profile_default/startup/10-pyinstrument.py
+++ -ipython/profile_default/startup/10-pyinstrument.py
@@ -1,13 +1,22 @@
-from IPython.core.magic import register_line_cell_magic
+from IPython.core.magic import no_var_expand, register_line_cell_magic
 
 
+@no_var_expand
 @register_line_cell_magic
 def pyinstrument(line, cell=None):
     """
     Magic commands %pyinstrument and %%pyinstrument.
     """
+    import datetime
+    from pathlib import Path
+
     from IPython import get_ipython
-    from pyinstrument import Profiler
+    from pyinstrument import Profiler, renderers
+
+    # Write to timestamped files under ~/pyinstrument.
+    now = datetime.datetime.now()
+    filename = now.strftime('~/pyinstrument/%Y%m%d.%H%M%S.%f.html')
+    path = Path(filename).expanduser()
 
     code = line if cell is None else cell
     shell = get_ipython()
@@ -15,9 +24,19 @@ def pyinstrument(line, cell=None):
     with Profiler() as profiler:
         shell.ex(code)
 
-    profiler.open_in_browser()
+    # Can't call profiler.write_html directly because it does not yet pass
+    # through show_all to HTMLRenderer in pyinstrument 4.6.1.
+    path.parent.mkdir(exist_ok=True)
+    path.write_text(
+        profiler.output(
+            renderer=renderers.HTMLRenderer(show_all=True, timeline=False)
+        ),
+        encoding='utf-8',
+    )
+    print(path)
 
 
 # Clean up local objects now that registration is complete.
+del no_var_expand
 del pyinstrument
 del register_line_cell_magic
